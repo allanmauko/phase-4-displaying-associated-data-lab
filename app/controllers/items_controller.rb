a415b1571ca067d1462bf6bf8c51c8b66f238b8a
allanmauko <allan.mauko@student.moringaschool.com>
2022-10-08 05:57:26 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+# rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+def index
+    items = Item.all.order(params[:id])
+    render json: items, include: :user
+end
+
 end
